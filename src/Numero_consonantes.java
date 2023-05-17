c722600b5d8a56a3c01129692696582bbe8550d0
Galleguin_mzz <sg9787188@gmail.com>
2023-05-17 14:19:37 +0200

    kdjkjdcbhjds

--- src/Numero_consonantes.java
+++ src/Numero_consonantes.java
@@ -1,4 +1,37 @@
 
 public class Numero_consonantes {
+	
+	private String frase;
 
+	public Numero_consonantes(String frase) {
+		super();
+		this.frase = frase;
+	}
+
+	public String getFrase() {
+		return frase;
+	}
+
+	public void setFrase(String frase) {
+		this.frase = frase;
+	}
+	
+	public int numero_Consonantes () {
+		
+		int cantidad=0;
+		for (int i=0;i<frase.length(); i++) {
+			if (frase.charAt(i)!='a' && frase.charAt(i)!='e' && frase.charAt(i)!='i' && frase.charAt(i)!='o' && frase.charAt(i)!='u') {
+				cantidad++;
+			}
+		}
+		
+		return cantidad;
+	}
+
+	@Override
+	public String toString() {
+		return "Numero_consonantes [frase=" + frase + "]";
+	}
+	
+	
 }
--- src/PrincipalApp_ACTIVIDADES.java
+++ src/PrincipalApp_ACTIVIDADES.java
@@ -1,4 +1,15 @@
+import java.util.*;
 
 public class PrincipalApp_ACTIVIDADES {
-
+	
+	static Scanner sc = new Scanner (System.in);
+	public static void main (String [] args) {
+		
+		int cantidad;
+		
+		Numero_consonantes nc = new Numero_consonantes("Hola, somos Tomas y Sergio");
+		cantidad = nc.numero_Consonantes();
+		
+		
+	}
 }
